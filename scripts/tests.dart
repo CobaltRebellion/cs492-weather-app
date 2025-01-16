79715188f7cf8a9c3b994266519e98b89bfded1f
Braxton <braxton.d.lowe@gmail.com>
2025-01-16 15:42:05 -0800

    Completed work for assignment2-2

--- scripts/tests.dart
+++ scripts/tests.dart
@@ -7,6 +7,17 @@ Future<void> main() async {
   // TODO: Seach for latitutes and longitudes of 5 cities in the US on the internet
   // Create a for loop that will generate forecasts arrays for each city
   // TODO: create forecasts and forecastsHourly both of type List<forecast.Forecast>
-  forecast.getForecastFromPoints(lat, lon);
-  forecast.getForecastHourlyFromPoints(lat,lon);
+  List<List<double>> coords = [
+    [44.05, -121.31],
+    [40.71, -74.006],
+    [41.878, -87.629],
+    [25.7617, -80.1918],
+    [35.0844, -106.65]
+  ];
+
+  for (List<double> coord in coords){
+    List<forecast.Forecast> forecasts = await forecast.getForecastFromPoints(coord[0], coord[1]);
+    List<forecast.Forecast> forecastsHourly = await forecast.getForecastHourlyFromPoints(coord[0], coord[1]);
+    print("uh oh");
+  }
 }
